0d64658c6a8dbc4d93f4389b3c535fd7a263980b
Thijs Tak <thijstak@gmail.com>
2016-12-10 23:06:25 +0100

    Changed collisions a bit and changed the direction of the mules.

--- Assets/Scripts/Units/Mule.cs
+++ Assets/Scripts/Units/Mule.cs
@@ -24,7 +24,10 @@ namespace Units
 
 		public void Update()
 		{
-
+			if (Body.velocity.x != 0 && Body.velocity.z != 0)
+			{
+				transform.rotation = Quaternion.LookRotation(new Vector3(Body.velocity.x, 0, Body.velocity.z));
+			}
 		}
 
 		public void FixedUpdate()
