8953594e126e40644757183767e9dd8d6f0f385e
Thijs Tak <thijstak@gmail.com>
2016-12-10 17:11:26 +0100

    Added Mule and base basics.

---
+++ Assets/Scripts/Behaviours/Mule/BehaviourState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Units;
+
+namespace Assets.Scripts.Behaviours.Mule
+{
+	public abstract class BehaviourState
+	{
+		public abstract bool Update(Units.Mule mule);
+		public abstract void Abort();
+	}
+}
---
+++ Assets/Scripts/Units/Base.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Units
+{
+	public class Base : BaseUnit
+	{
+		public Transform DropPoint;
+	}
+}
