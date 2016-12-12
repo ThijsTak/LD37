2fed4f0973af52afe25333e7e12393b991a177c3
Paul <paul+gitkraken@asamco.com>
2016-12-12 16:00:38 +0100

    create basics for main menu

---
+++ Assets/Scripts/MainMenu/MouseClick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MouseClick : MonoBehaviour {
+
+	public bool isStart;
+	public bool isQuit;
+
+	void OnMouseUp()
+	{
+		if (isStart)
+		{
+			SceneManager.LoadScene(1);
+		}
+		else if (isQuit)
+		{
+			Application.Quit();
+		}
+	}
+}
