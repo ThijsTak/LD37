718e597eaf6dc4ff6fc173549c134b58825fce03
Paul <paul+gitkraken@asamco.com>
2016-12-12 20:35:02 +0100

    added invincebilityTime

--- Assets/Scripts/Behaviours/DamageOnImpact.cs
+++ Assets/Scripts/Behaviours/DamageOnImpact.cs
@@ -11,16 +11,27 @@ namespace Assets.Scripts.Behaviours
 	{
 		public float Damage = 2.5f;
 		public float Force = 5.0f;
+		public float InvincibilityTime = 1000;
+		private float hitTime = 0;
+
+		void Update()
+		{
+			if (hitTime > 0)
+			{
+				hitTime -= Time.deltaTime*1000;
+			}
+		}
 
 		void OnTriggerEnter(Collider collider)
 		{
-			if (collider.gameObject.tag == "Player")
+			if (collider.gameObject.tag == "Player" && hitTime <= 0)
 			{
 				Player player = collider.gameObject.GetComponent<Player>();
 				player.DrainEnergy(Damage);
 				player.body.AddForce(
 					(player.transform.position - gameObject.transform.position).normalized * Force, 
-					ForceMode.VelocityChange); 
+					ForceMode.VelocityChange);
+				hitTime = InvincibilityTime;
 			}
 		}
 	}
