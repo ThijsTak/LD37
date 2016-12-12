33c8422f5d56f5ef9de02b2fba168f06883cc90b
Thijs Tak <thijstak@gmail.com>
2016-12-12 14:44:08 +0100

    Scene and build change.

--- Assets/Scripts/Helpers/SceneHelpers.cs
+++ Assets/Scripts/Helpers/SceneHelpers.cs
@@ -8,8 +8,9 @@ namespace Helpers
 	public static class SceneHelper
 	{
 		public const int MainMenu = 0;
-		public const int Game = 1;
-		public const int Victory = 2;
-		public const int GameOver = 3;
+		public const int Intro = 1;
+		public const int Game = 2;
+		public const int Victory = 3;
+		public const int GameOver = 4;
 	}
 }
